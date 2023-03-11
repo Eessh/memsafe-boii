fc4e9375b00e439f3201da4da54a9d34056d4cba
Eessh <eesshkumarjagana@gmail.com>
2023-03-11 17:29:04 +0530

    moved functions required for implementation from header file to source file

--- memsafe_boii.c
+++ memsafe_boii.c
@@ -1,7 +1,80 @@
-#include "memsafe_boii.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "memsafe_boii.h"
+
+/**
+ * This is the allocation record, which will be stored
+ * in a linked list of allocation records.
+ */
+typedef struct memsafe__boii__allocation
+{
+  void* mem_address;
+  size_t mem_size;
+  char* func_call_file;
+  size_t func_call_line;
+  char* func_name;
+
+  struct memsafe__boii__allocation* next;
+} memsafe__boii__allocation;
+
+/**
+ * This is the linked list for the allocation records.
+ */
+typedef struct memsafe__boii__allocation_list
+{
+  memsafe__boii__allocation* head;
+  memsafe__boii__allocation* tail;
+} memsafe__boii__allocation_list;
+
+/**
+ * Creates/allocates memory for a new allocation record.
+ *
+ * Returns `NULL` if unable to create record.
+ */
+static memsafe__boii__allocation*
+memsafe__boii__allocation_new(void* mem_address,
+                              size_t mem_size,
+                              const char* func_call_file,
+                              size_t func_call_line,
+                              const char* func_name);
+
+/**
+ * Frees/deallocates memory for the allocation record.
+ */
+static void memsafe__boii__allocation_free(memsafe__boii__allocation* record);
+
+/**
+ * Creates/allocates memory for a new allocation record list.
+ *
+ * Returns `NULL` if unable to create new allocation record list.
+ */
+static memsafe__boii__allocation_list* memsafe__boii__allocation_list_new();
+
+/**
+ * Adds the new allocation record to the record list.
+ */
+static void
+memsafe__boii__allocation_list_add_record(memsafe__boii__allocation_list* list,
+                                          memsafe__boii__allocation* record);
+
+/**
+ * Removes the allocation record for the address pointed by the entity.
+ */
+static void memsafe__boii__allocation_list_remove_record(
+  memsafe__boii__allocation_list* list, void* entity);
+
+/**
+ * Frees/deallocates memory for allocation record list.
+ */
+static void
+memsafe__boii__allocation_list_free(memsafe__boii__allocation_list* list);
+
+/**
+ * Actual variable which holds the records.
+ */
+static memsafe__boii__allocation_list memory_allocations_record_list =
+  (memsafe__boii__allocation_list){.head = NULL, .tail = NULL};
 
 static memsafe__boii__allocation*
 memsafe__boii__allocation_new(void* mem_address,
